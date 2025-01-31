86be7e0e480e147e3d2f03f776aca2332f3252c8
Mateus H.O.S <ma0987654321mateus@gmail.com>
2025-01-31 17:50:06 -0300

    Autoloading, Namespaces e Models

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,25 @@
 <?php
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\job;
+
+ 
+
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function (){
     return view('jobs',[
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'R$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'R$10,000'
-            ],
-            [
-                'id' => 3,
-                 'title' => 'Teacher',
-                 'salary' => 'R$40,000'
-            ]
-        ]
-    ]);
+        'jobs' => job::all()
+        ]);
 });
 
 Route::get('/jobs/{id}',function($id){
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'R$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'R$10,000'
-            ],
-            [
-                'id' => 3,
-                 'title' => 'Teacher',
-                 'salary' => 'R$40,000'
-            ]
-            ];
+    
+    $job = job::find($id);
 
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id); 
-            
     return view('job', ['job' => $job]);
 });
 
